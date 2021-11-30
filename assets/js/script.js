86b1fd91ef57a651350a927e2eb5f772cb5e9a92
Hector Nava <hector.r.nava44@gmail.com>
2021-11-30 16:25:20 -0700

    Added function ready and jquery UI CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,42 @@
-let charList = [];
+$(document).ready(function() {
+  const $searchInput = $('#input');
+  let charList = [];
 
-retrieveChar(1).then(dataLength => {
-  for (let i = 2; i <= dataLength; i++) {
-    retrieveChar(i);
-  };
-});
-
-function retrieveChar(index) {
-  return $.ajax({
-    method: 'GET',
-    url: 'https://api.disneyapi.dev/characters?page=' + index
-  }).then(function(response) {
-    for (let i = 0; i < response.data.length; i++) {
-      charList.push(response.data[i].name);
+  retrieveChar(1).then(dataLength => {
+    for (let i = 2; i <= dataLength; i++) {
+      retrieveChar(i);
     };
-
-    return response.totalPages;
-  }).catch(function(error) {
-    console.log(error);
   });
-};
 
-  // $.ajax({
-  //   method: 'GET',
-  //   url: 'https://imdb-api.com/en/API/SearchMovie/k_bicys5i4/Tangled'
-  // }).then(function(response) {
-  //   console.log(response);
-  // }).catch(function(error) {
-  //   console.log(error);
-  // });
+  $(function() {
+    $searchInput.autocomplete({
+      minLength: 3,
+      source: charList
+    });
+  });
+
+  function retrieveChar(index) {
+    return $.ajax({
+      method: 'GET',
+      url: 'https://api.disneyapi.dev/characters?page=' + index
+    }).then(function(response) {
+      for (let i = 0; i < response.data.length; i++) {
+        charList.push(response.data[i].name);
+      };
+
+      return response.totalPages;
+    }).catch(function(error) {
+      console.log(error);
+    });
+  };
+
+    // $.ajax({
+    //   method: 'GET',
+    //   url: 'https://imdb-api.com/en/API/SearchMovie/k_bicys5i4/Tangled'
+    // }).then(function(response) {
+    //   console.log(response);
+    // }).catch(function(error) {
+    //   console.log(error);
+    // });
+
+});
