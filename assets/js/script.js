92d28c5f740cc8ecde5cdd4c7bd9a3256d10066f
Hector Nava <hector.r.nava44@gmail.com>
2021-11-30 16:21:09 -0700

    Added loop to get all characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,31 @@
-$.ajax({
-    method: 'GET',
-    url: 'https://api.disneyapi.dev/characters/308'
-  }).then(function(response) {
-    console.log(response);
-  }).catch(function(error) {
-    console.log(error);
-  });
+let charList = [];
 
-  $.ajax({
+retrieveChar(1).then(dataLength => {
+  for (let i = 2; i <= dataLength; i++) {
+    retrieveChar(i);
+  };
+});
+
+function retrieveChar(index) {
+  return $.ajax({
     method: 'GET',
-    url: 'https://imdb-api.com/en/API/SearchMovie/k_bicys5i4/Tangled'
+    url: 'https://api.disneyapi.dev/characters?page=' + index
   }).then(function(response) {
-    console.log(response);
+    for (let i = 0; i < response.data.length; i++) {
+      charList.push(response.data[i].name);
+    };
+
+    return response.totalPages;
   }).catch(function(error) {
     console.log(error);
   });
+};
+
+  // $.ajax({
+  //   method: 'GET',
+  //   url: 'https://imdb-api.com/en/API/SearchMovie/k_bicys5i4/Tangled'
+  // }).then(function(response) {
+  //   console.log(response);
+  // }).catch(function(error) {
+  //   console.log(error);
+  // });
